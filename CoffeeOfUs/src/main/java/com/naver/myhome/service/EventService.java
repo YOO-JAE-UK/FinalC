5996f1b7b614106161590d1021e9e1d31da18ad5
gangmoon <gangmoon@192.168.35.104>
2021-09-30 22:07:53 +0900

    추가 강문

---
+++ CoffeeOfUs/src/main/java/com/naver/myhome/service/EventService.java
@@ -0,0 +1,18 @@
+package com.naver.myhome.service;
+
+import java.util.List;
+
+import com.naver.myhome.domain.AttendCheckMember;
+
+public interface EventService {
+
+	public int add(String id);
+
+	public List<AttendCheckMember> getList(String id);
+
+	public  AttendCheckMember  isCheck(String id,String attenddate);
+
+	public int attendCount(String id, String yearMonth);
+
+}
+ 
